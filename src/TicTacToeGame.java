546c5e8dc26ea4de781c0edd4c545fa66fe18307
Harlan Howe <hhowe7@gatech.edu>
2023-08-30 14:34:11 -0500

    Created member variables and initialized them.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,12 +1,27 @@
 public class TicTacToeGame
 {
     // create class variables here....
+    private String square1, square2, square3, square4, square5;
+    private String square6, square7, square8, square9;
+    private String whoseTurn;
+    private boolean keepPlaying;
 
     /**
      * Constructor
      */
     public TicTacToeGame()
     {
+        square1 = "1";
+        square2 = "2";
+        square3 = "3";
+        square4 = "4";
+        square5 = "5";
+        square6 = "6";
+        square7 = "7";
+        square8 = "8";
+        square9 = "9";
+        whoseTurn = "X";
+        keepPlaying = true;
 
     }
 
